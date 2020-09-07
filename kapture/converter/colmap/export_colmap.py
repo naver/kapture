4513e886ebe7e23ddfc8a27d10b9c8d5dccbca4a
Philippe Rerole <Philippe.Rerole@naverlabs.com>
2020-09-07 17:31:43 +0200

    Externalized some code to simplify export_colmap. Fixed some typos.

--- kapture/converter/colmap/export_colmap.py
+++ kapture/converter/colmap/export_colmap.py
@@ -13,9 +13,10 @@ import warnings
 # kapture
 import kapture
 from kapture.core.Trajectories import rigs_remove_inplace
+import kapture.algo.merge_keep_ids
 import kapture.io.csv as csv
 import kapture.io.structure
-import kapture.algo.merge_keep_ids
+import kapture.utils.paths
 
 # local
 from .database import COLMAPDatabase
@@ -27,32 +28,27 @@ from .export_colmap_reconstruction import export_to_colmap_txt
 logger = logging.getLogger('colmap')
 
 
-def export_colmap(kapture_dirpath: str,
+def export_colmap(kapture_dir_path: str,
                   colmap_database_filepath: str,
-                  colmap_reconstruction_dirpath: Optional[str],
+                  colmap_reconstruction_dir_path: Optional[str],
                   colmap_rig_filepath: str = None,
                   force_overwrite_existing: bool = False) -> None:
     """
     Exports kapture data to colmap database and or reconstruction text files.
 
-    :param kapture_dirpath: kapture top directory
+    :param kapture_dir_path: kapture top directory
     :param colmap_database_filepath: path to colmap database file
-    :param colmap_reconstruction_dirpath: path to colmap reconstruction directory
+    :param colmap_reconstruction_dir_path: path to colmap reconstruction directory
     :param colmap_rig_filepath: path to colmap rig file
     :param force_overwrite_existing: Silently overwrite colmap files if already exists.
     """
 
     os.makedirs(path.dirname(colmap_database_filepath), exist_ok=True)
-    if colmap_reconstruction_dirpath:
-        os.makedirs(colmap_reconstruction_dirpath, exist_ok=True)
+    if colmap_reconstruction_dir_path:
+        os.makedirs(colmap_reconstruction_dir_path, exist_ok=True)
 
     assert colmap_database_filepath
-    if path.isfile(colmap_database_filepath):
-        to_delete = force_overwrite_existing or (input(
-            'database file already exist, would you like to delete it ? [y/N]').lower() == 'y')
-        if to_delete:
-            logger.info('deleting already existing {}'.format(colmap_database_filepath))
-            os.remove(colmap_database_filepath)
+    kapture.utils.paths.safe_remove_file(colmap_database_filepath, force_overwrite_existing, 'colmap database')
 
     logger.info('creating colmap database in {}'.format(colmap_database_filepath))
     db = COLMAPDatabase.connect(colmap_database_filepath)
@@ -60,7 +56,7 @@ def export_colmap(kapture_dirpath: str,
         raise ValueError('the existing colmap database is not empty : {}'.format(colmap_database_filepath))
 
     logger.info('loading kapture files...')
-    kapture_data = csv.kapture_from_dir(kapture_dirpath)
+    kapture_data = csv.kapture_from_dir(kapture_dir_path)
     assert isinstance(kapture_data, kapture.Kapture)
 
     # COLMAP does not fully support rigs.
@@ -70,18 +66,17 @@ def export_colmap(kapture_dirpath: str,
         rigs_remove_inplace(kapture_data.trajectories, kapture_data.rigs)
 
     # write colmap database
-    kapture_to_colmap(kapture_data, kapture_dirpath, db)
+    kapture_to_colmap(kapture_data, kapture_dir_path, db)
 
-    if colmap_reconstruction_dirpath:
+    if colmap_reconstruction_dir_path:
         # create text files
         colmap_camera_ids = get_colmap_camera_ids_from_db(db, kapture_data.records_camera)
         colmap_image_ids = get_colmap_image_ids_from_db(db)
-        export_to_colmap_txt(
-            colmap_reconstruction_dirpath=colmap_reconstruction_dirpath,
-            kapture_data=kapture_data,
-            kapture_dirpath=kapture_dirpath,
-            colmap_camera_ids=colmap_camera_ids,
-            colmap_image_ids=colmap_image_ids)
+        export_to_colmap_txt(colmap_reconstruction_dir_path,
+                             kapture_data,
+                             kapture_dir_path,
+                             colmap_camera_ids,
+                             colmap_image_ids)
         if colmap_rig_filepath:
             try:
                 if kapture_data.rigs is None:
