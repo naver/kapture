ea75b3745d784151eb90d8420e1d1dd36b5ed861
Martin Humenberger <martin.humenberger@naverlabs.com>
2021-03-09 16:59:02 +0100

    added depth map registration for 7-scenes

--- tools/kapture_import_7scenes.py
+++ tools/kapture_import_7scenes.py
@@ -23,6 +23,7 @@ import quaternion
 from typing import Optional
 from PIL import Image
 from tqdm import tqdm
+import quaternion
 # kapture
 import path_to_kapture  # noqa: F401
 import kapture
@@ -41,11 +42,48 @@ RGB_SUFFIX = 'color'
 DEPTH_SUFFIX = 'depth'
 RGB_SENSOR_ID = 'kinect_rgb'
 DEPTH_SENSOR_ID = 'kinect_depth'
+REG_DEPTH_SENSOR_ID = 'kinect_depth_reg'
 RGBD_SENSOR_ID = 'kinect'
 PARTITION_FILENAMES = {
     'mapping': 'TrainSplit.txt',
     'query': 'TestSplit.txt'
 }
+
+
+def get_camera_matrix(fx: float, fy: float, cx: float, cy: float):
+    return np.array([[fx, 0, cx],
+                     [0, fy, cy],
+                     [0, 0, 1]])
+
+
+def get_K(camera_type: kapture.CameraType, camera_params):
+    if camera_type == kapture.CameraType.UNKNOWN_CAMERA:
+        focal = 1.2 * max(width, height)
+        return get_camera_matrix(focal, focal, camera_params[0] / 2, camera_params[1] / 2)
+    elif camera_type in [kapture.CameraType.PINHOLE, kapture.CameraType.OPENCV,
+                         kapture.CameraType.OPENCV_FISHEYE, kapture.CameraType.FULL_OPENCV,
+                         kapture.CameraType.FOV, kapture.CameraType.THIN_PRISM_FISHEYE]:
+        return get_camera_matrix(camera_params[2], camera_params[3], camera_params[4], camera_params[5])
+    else:
+        return get_camera_matrix(camera_params[2], camera_params[3], camera_params[4], camera_params[4])
+
+
+def register_depth(Kdepth, Kcolor, Rt, depth, width_color, height_color):
+    reg_depth = np.zeros((height_color, width_color), dtype=np.float32)
+    y, x = np.meshgrid(np.arange(depth.shape[0]), np.arange(depth.shape[1]), indexing='ij')
+    x = x.reshape(1, -1)
+    y = y.reshape(1, -1)
+    z = depth.reshape(1, -1)
+    x = (x - Kdepth[0, 2]) / Kdepth[0, 0]
+    y = (y - Kdepth[1, 2]) / Kdepth[1, 1]
+    pts = np.vstack((x * z, y * z, z))
+    pts = Rt[:3, :3] @ pts + Rt[:3, 3:]
+    pts = Kcolor @ pts
+    px = np.round(pts[0, :] / pts[2, :])
+    py = np.round(pts[1, :] / pts[2, :])
+    mask = (px >= 0) * (py >= 0) * (px < width_color) * (py < height_color)
+    reg_depth[py[mask].astype(int), px[mask].astype(int)] = pts[2, mask]
+    return reg_depth
 
 
 def import_7scenes(d7scenes_path: str,
@@ -95,7 +133,6 @@ def import_7scenes(d7scenes_path: str,
         # read the authors split file
         partition_filepath = path.join(d7scenes_path, PARTITION_FILENAMES[partition])
         if not path.isfile(partition_filepath):
-
             raise FileNotFoundError(f'partition file is missing: {partition_filepath}.')
         with open(partition_filepath, 'rt') as file:
             split_sequences = [f'seq-{int(seq.strip()[len("sequence"):]):02}' for seq in file.readlines()]
@@ -123,6 +160,8 @@ def import_7scenes(d7scenes_path: str,
         snapshots[shot['timestamp'], RGB_SENSOR_ID] = shot['color']
         kapture_depth_map_filename = shot['depth'][:-len('.png')]  # kapture depth files are not png
         depth_maps[shot['timestamp'], DEPTH_SENSOR_ID] = kapture_depth_map_filename
+        kapture_registered_depth_map_filename = shot['depth'][:-len('.png')] + '.reg'  # kapture depth files are not png
+        depth_maps[shot['timestamp'], REG_DEPTH_SENSOR_ID] = kapture_registered_depth_map_filename
 
     # poses
     logger.info('import poses files ...')
@@ -141,27 +180,51 @@ def import_7scenes(d7scenes_path: str,
     """
     From authors: The RGB and depth camera have not been calibrated and we can’t provide calibration parameters at the
     moment. The recorded frames correspond to the raw, uncalibrated camera images. In the KinectFusion pipeline we used
-    the following default intrinsics for the depth camera: Principle point (320,240), Focal length (585,585).
+    the following default intrinsics for the depth camera: Principle point (320,240), Focal length (585,585).    
+    ----
+    We use the extr. kinect camera parameters from https://projet.liris.cnrs.fr/voir/activities-dataset/kinect-calibration.html. 
     """
     sensors = kapture.Sensors()
+    # camera_type = kapture.CameraType.OPENCV
+    # camera_params = [640, 480, 5.2161910696979987e+02, 5.2132946256749767e+02, 3.1755491910920682e+02, 2.5921654718027673e+02,
+    #                  2.5673002693536984e-01, -9.3976085633794137e-01, -1.8605549188751580e-03, -2.2232238578189420e-03]  # w, h, f, cx, cy, k1, k2, p1, p2, k3
     camera_type = kapture.CameraType.SIMPLE_PINHOLE
-    camera_params = [640, 480, 585, 320, 240]  # w, h, f, cx, cy
+    # camera_params = [640, 480, 5.2161910696979987e+02, 5.2132946256749767e+02, 3.1755491910920682e+02, 2.5921654718027673e+02]  # w, h, fx, fy, cx, cy
+    camera_params = [640, 480, 525, 320, 240]  # w, h, f, cx, cy
     sensors[RGB_SENSOR_ID] = kapture.Camera(
         name=RGB_SENSOR_ID,
         camera_type=camera_type,
         camera_params=camera_params
     )
+    # depth_camera_type = kapture.CameraType.OPENCV
+    # depth_camera_params = [640, 480, 5.8818670481438744e+02, 5.8724220649505514e+02, 3.1076280589210484e+02, 2.2887144980135292e+02,
+    #                        -1.8932947734719333e-01, 1.1358015104098631e+00, -4.4260345347128536e-03, -5.4869578635708153e-03, -2.2460143607712921e+00] # w, h, f, cx, cy, k1, k2, p1, p2, k3
+    depth_camera_type = kapture.CameraType.SIMPLE_PINHOLE
+    # depth_camera_params = [640, 480, 5.8818670481438744e+02, 5.8724220649505514e+02, 3.1076280589210484e+02, 2.2887144980135292e+02] # w, h, fx, fy, cx, cy
+    depth_camera_params = [640, 480, 585, 320, 240]  # w, h, f, cx, cy
     sensors[DEPTH_SENSOR_ID] = kapture.Camera(
         name=DEPTH_SENSOR_ID,
-        camera_type=camera_type,
+        camera_type=depth_camera_type,
+        camera_params=depth_camera_params,
+        sensor_type='depth'
+    )
+    sensors[REG_DEPTH_SENSOR_ID] = kapture.Camera(
+        name=REG_DEPTH_SENSOR_ID,
+        camera_type=depth_camera_type,
         camera_params=camera_params,
         sensor_type='depth'
     )
 
     # bind camera and depth sensor into a rig
+    R = np.array([[9.9996518012567637e-01, 2.6765126468950343e-03, -7.9041012313000904e-03],
+                  [-2.7409311281316700e-03, 9.9996302803027592e-01, -8.1504520778013286e-03],
+                  [7.8819942130445332e-03, 8.1718328771890631e-03, 9.9993554558014031e-01]])
+    T = np.array([-2.5558943178152542e-02, 1.0109636268061706e-04, 2.0318321729487039e-03])
+    Rt = np.vstack((np.hstack((R, T.reshape(3, 1))), np.array([0, 0, 0, 1])))
     logger.info('building rig with camera and depth sensor ...')
     rigs = kapture.Rigs()
-    rigs[RGBD_SENSOR_ID, RGB_SENSOR_ID] = kapture.PoseTransform()
+    rigs[RGBD_SENSOR_ID, RGB_SENSOR_ID] = kapture.PoseTransform(quaternion.from_rotation_matrix(R), T)
+    rigs[RGBD_SENSOR_ID, REG_DEPTH_SENSOR_ID] = kapture.PoseTransform(quaternion.from_rotation_matrix(R), T)
     rigs[RGBD_SENSOR_ID, DEPTH_SENSOR_ID] = kapture.PoseTransform()
 
     # import (copy) image files.
@@ -174,6 +237,8 @@ def import_7scenes(d7scenes_path: str,
     depth_map_filenames = kapture.io.records.records_to_filepaths(depth_maps, kapture_dir_path)
     hide_progress = logger.getEffectiveLevel() > logging.INFO
     for depth_map_filename, depth_map_filepath_kapture in tqdm(depth_map_filenames.items(), disable=hide_progress):
+        if '.reg' in depth_map_filename:
+            continue
         depth_map_filepath_7scenes = path.join(d7scenes_path, depth_map_filename + '.png')
         depth_map = np.array(Image.open(depth_map_filepath_7scenes))
         # change invalid depth from 65535 to 0
@@ -181,6 +246,10 @@ def import_7scenes(d7scenes_path: str,
         # depth maps is in mm in 7scenes, convert it to meters
         depth_map = depth_map.astype(np.float32) * 1.0e-3
         kapture.io.records.records_depth_to_file(depth_map_filepath_kapture, depth_map)
+        # register depth to rgb
+        reg_depth_map = register_depth(get_K(depth_camera_type, depth_camera_params), get_K(camera_type, camera_params),
+                                       Rt, depth_map, camera_params[0], camera_params[1])
+        kapture.io.records.records_depth_to_file(depth_map_filepath_kapture + '.reg', reg_depth_map)
 
     # pack into kapture format
     imported_kapture = kapture.Kapture(
