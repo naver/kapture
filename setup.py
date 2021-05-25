6f1d97ebb4944dfcf25b5c5227b641681348f843
Nicolas Guerin <nicolas.guerin@naverlabs.com>
2021-05-25 23:39:42 +0200

    More testing

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ long_description = read_doc(readme_filepath)
 setuptools.setup(
     # description
     name='kapture',
-    version="1.1.1.test",
+    version="1.1.1b4",
     author="naverlabs",
     author_email="kapture@naverlabs.com",
     description="kapture: file format for SfM",
