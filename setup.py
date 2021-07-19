fe32511e7791eb6d5081bdfebb6946d19029fd70
Yohann CABON <yohanncabon@gmail.com>
2021-07-19 09:55:17 +0200

    change version to 1.1.2

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ long_description = read_doc(readme_filepath)
 setuptools.setup(
     # description
     name='kapture',
-    version="1.1.1",
+    version="1.1.2",
     author="naverlabs",
     author_email="kapture@naverlabs.com",
     description="kapture: file format for SfM",
