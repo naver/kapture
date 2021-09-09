9d4e417d070bed9f021c86da465052e27e08f5ea
Philippe Rerole <Philippe.Rerole@naverlabs.com>
2021-09-09 13:42:53 +0200

    Renamed tests and test data for import_images_list_with_poses

--- tests/test_image_list_with_poses.py
+++ tests/test_image_list_with_poses.py
@@ -10,14 +10,14 @@ import path_to_kapture  # enables import kapture  # noqa: F401
 from kapture.algo.compare import equal_kapture
 from kapture.io.csv import kapture_from_dir
 from kapture.io.records import TransferAction
-from tools.kapture_import_localized_images import import_localized_images
+from tools.kapture_import_image_list_with_poses import import_image_list_with_poses
 
 
 class TestImageList(unittest.TestCase):
 
     def setUp(self):
         self._samples_folder = path.abspath(path.join(path.dirname(__file__),  '../samples/cour_carree_markers/'))
-        self._localized_file = path.join(self._samples_folder, 'map/localized_images.txt')
+        self._images_list_with_poses_file = path.join(self._samples_folder, 'map/images_with_poses.txt')
         self._images_folder = path.join(self._samples_folder, 'map')
         self._camera_model_path = path.join(path.join(self._samples_folder, 'map'), 's10.yml')
         self._tempdir = tempfile.TemporaryDirectory()
@@ -25,18 +25,18 @@ class TestImageList(unittest.TestCase):
         os.makedirs(self._kapture_path, exist_ok=True)
 
     def test_import_images_missing_file(self):
-        self.assertRaises(FileNotFoundError, import_localized_images,
+        self.assertRaises(FileNotFoundError, import_image_list_with_poses,
                           '', self._images_folder, self._camera_model_path,
                           self._kapture_path, True, TransferAction.copy)
 
     def test_import_images_missing_dir(self):
-        self.assertRaises(ValueError, import_localized_images,
-                          self._localized_file, '', self._camera_model_path,
+        self.assertRaises(ValueError, import_image_list_with_poses,
+                          self._images_list_with_poses_file, '', self._camera_model_path,
                           self._kapture_path, True, TransferAction.copy)
 
     def test_import_with_images(self):
-        import_localized_images(self._localized_file, self._images_folder, self._camera_model_path,
-                                self._kapture_path, True, TransferAction.copy)
+        import_image_list_with_poses(self._images_list_with_poses_file, self._images_folder, self._camera_model_path,
+                                     self._kapture_path, True, TransferAction.copy)
 
         ref_kapture_path = path.join(self._samples_folder, 'kapture_images')
         expected_kapture = kapture_from_dir(ref_kapture_path)
@@ -44,8 +44,8 @@ class TestImageList(unittest.TestCase):
         self.assertTrue(equal_kapture(imported_kapture, expected_kapture))
 
     def test_import_images_no_camera(self):
-        import_localized_images(self._localized_file, self._images_folder, '',
-                                self._kapture_path, True, TransferAction.copy)
+        import_image_list_with_poses(self._images_list_with_poses_file, self._images_folder, '',
+                                     self._kapture_path, True, TransferAction.copy)
 
         ref_kapture_path = path.join(self._samples_folder, 'kapture_images_no_camera')
         expected_kapture = kapture_from_dir(ref_kapture_path)
@@ -53,8 +53,8 @@ class TestImageList(unittest.TestCase):
         self.assertTrue(equal_kapture(imported_kapture, expected_kapture))
 
     def test_import_no_images(self):
-        import_localized_images(self._localized_file, '', self._camera_model_path,
-                                self._kapture_path, True, TransferAction.copy, True)
+        import_image_list_with_poses(self._images_list_with_poses_file, '', self._camera_model_path,
+                                     self._kapture_path, True, TransferAction.copy, True)
 
         ref_kapture_path = path.join(self._samples_folder, 'kapture_no_images')
         expected_kapture = kapture_from_dir(ref_kapture_path)
@@ -62,8 +62,8 @@ class TestImageList(unittest.TestCase):
         self.assertTrue(equal_kapture(imported_kapture, expected_kapture))
 
     def test_import_no_images_nor_camera(self):
-        import_localized_images(self._localized_file, '', '',
-                                self._kapture_path, True, TransferAction.copy, True)
+        import_image_list_with_poses(self._images_list_with_poses_file, '', '',
+                                     self._kapture_path, True, TransferAction.copy, True)
 
         ref_kapture_path = path.join(self._samples_folder, 'kapture_no_images_nor_camera')
         expected_kapture = kapture_from_dir(ref_kapture_path)
