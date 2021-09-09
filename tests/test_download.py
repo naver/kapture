a6df0934b4426b7bf7a9838cc88ff9b1fa8a4037
julien.morat <julien.morat@naverlabs.com>
2021-09-09 10:43:32 +0200

    remove dl test: breaking index file when not committed yet.

--- tests/test_download.py
+++ tests/test_download.py
@@ -17,23 +17,23 @@ Returns error if a dataset is not reachable
 
 SLOW_TESTS = os.environ.get('SLOW_TESTS', False)
 
-
-class TestDownload(unittest.TestCase):
-
-    def setUp(self):
-        self.dataset_dir = path.abspath(path.join(path.dirname(__file__),  '../dataset'))
-
-    def test_update(self):
-        test_args = ["downloader", "--install_path", self.dataset_dir, "update"]
-        with patch.object(sys, 'argv', test_args):
-            self.assertEqual(download.kapture_download_dataset_cli(), 0)
-
-    @unittest.skipUnless(SLOW_TESTS, "slow test")
-    def test_all_datasets(self):
-        test_args = ["downloader", "--install_path", self.dataset_dir, "list", "--full"]
-        with patch.object(sys, 'argv', test_args):
-            self.assertEqual(download.kapture_download_dataset_cli(), 0)
-
+#
+# class TestDownload(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.dataset_dir = path.abspath(path.join(path.dirname(__file__),  '../dataset'))
+#
+#     def test_update(self):
+#         test_args = ["downloader", "--install_path", self.dataset_dir, "update"]
+#         with patch.object(sys, 'argv', test_args):
+#             self.assertEqual(download.kapture_download_dataset_cli(), 0)
+#
+#     @unittest.skipUnless(SLOW_TESTS, "slow test")
+#     def test_all_datasets(self):
+#         test_args = ["downloader", "--install_path", self.dataset_dir, "list", "--full"]
+#         with patch.object(sys, 'argv', test_args):
+#             self.assertEqual(download.kapture_download_dataset_cli(), 0)
+#
 
 if __name__ == '__main__':
     unittest.main()
