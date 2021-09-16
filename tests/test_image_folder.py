4707d968207f58c0cd3346d7e9e6edc523c2636b
julien.morat <julien.morat@naverlabs.com>
2021-09-16 13:31:42 +0200

    fix tests with existing data

--- tests/test_image_folder.py
+++ tests/test_image_folder.py
@@ -16,10 +16,8 @@ from tools.kapture_import_image_folder import import_image_folder
 
 class TestImageFolder(unittest.TestCase):
     def setUp(self):
-        samples_folder = path.abspath(path.join(path.dirname(__file__),  '../samples/'))
-        self.maupertuis_folder = path.join(samples_folder, 'SceauxCastle')
-        self.images_folder = path.join(self.maupertuis_folder, 'images2')
-        self.kapture_folder = path.join(self.maupertuis_folder, 'kapture')
+        samples_folder = path.abspath(path.join(path.dirname(__file__),  '../samples/Aachen-Day-Night'))
+        self.images_folder = path.join(samples_folder, 'images_upright', 'query', 'day')
 
     def test_import_image_folder(self):
         with tempfile.TemporaryDirectory() as tmpdirname:
@@ -33,16 +31,16 @@ class TestImageFolder(unittest.TestCase):
 
         # sensors
         sensors = list(kapture.flatten(kapture_result.sensors, is_sorted=True))
-        self.assertEqual(11, len(sensors))
+        self.assertEqual(4, len(sensors))
         first_sensor = sensors[0][1]
         self.assertTrue(isinstance(first_sensor, kapture.Sensor))
         self.assertTrue(isinstance(first_sensor, kapture.Camera))
         self.assertEqual(kapture.CameraType.UNKNOWN_CAMERA, first_sensor.camera_type)
         # images
         images = list(kapture.flatten(kapture_result.records_camera, is_sorted=True))
-        self.assertEqual(11, len(images))
+        self.assertEqual(4, len(images))
         first_image = images[0]
-        self.assertEqual((0, 'sensor0', 'a/100_7100.JPG'), first_image)
+        self.assertEqual((0, 'sensor0', 'milestone/2011-12-17_14-28-15_257.jpg'), first_image)
 
     def test_import_image_folder_with_single_sensor(self):
         # creates a folder with both images and sensors.txt
@@ -50,9 +48,11 @@ class TestImageFolder(unittest.TestCase):
             # copy images
             dir_util.copy_tree(self.images_folder, tmpdirname_in)
             # copy sensors.txt
-            sensors_file_path_src = get_csv_fullpath(kapture.Sensors, self.kapture_folder)
-            sensors_file_path_dst = path.join(tmpdirname_in, 'sensors.txt')
-            file_util.copy_file(sensors_file_path_src, sensors_file_path_dst)
+            sensors_file_path = path.join(tmpdirname_in, 'sensors.txt')
+            with open(sensors_file_path, 'wt') as f:
+                f.write("""
+                cam0, , camera, RADIAL, 2832, 2128, 2986.7801886485645, 1463.2779641275515, 1112.2674656244137, -0.2465279459531899, 0.2675790139425493
+                """)
 
             import_image_folder(
                 images_path=tmpdirname_in,
@@ -67,15 +67,15 @@ class TestImageFolder(unittest.TestCase):
         self.assertEqual(1, len(sensors))
         camera_id = sensors[0][0]
         camera = sensors[0][1]
-        self.assertEqual('0', camera_id)
+        self.assertEqual('cam0', camera_id)
         self.assertTrue(isinstance(camera, kapture.Sensor))
         self.assertTrue(isinstance(camera, kapture.Camera))
         self.assertEqual(kapture.CameraType.RADIAL, camera.camera_type)
         # images
         images = list(kapture.flatten(kapture_result.records_camera, is_sorted=True))
-        self.assertEqual(11, len(images))
+        self.assertEqual(4, len(images))
         first_image = images[0]
-        self.assertEqual((0, '0', 'a/100_7100.JPG'), first_image)
+        self.assertEqual((0, 'cam0', 'milestone/2011-12-17_14-28-15_257.jpg'), first_image)
 
     def test_import_image_folder_with_multiple_sensors(self):
         # creates a folder with both images and sensors.txt
@@ -86,8 +86,8 @@ class TestImageFolder(unittest.TestCase):
             sensors_file_path = path.join(tmpdirname_in, 'sensors.txt')
             with open(sensors_file_path, 'wt') as f:
                 f.write("""
-                a, , camera, RADIAL, 2832, 2128, 2986.7801886485645, 1463.2779641275515, 1112.2674656244137, -0.2465279459531899, 0.2675790139425493
-                b, , camera, RADIAL, 2832, 2128, 2986.7801886485645, 1463.2779641275515, 1112.2674656244137, -0.2465279459531899, 0.2675790139425493
+                milestone, , camera, RADIAL, 2832, 2128, 2986.7801886485645, 1463.2779641275515, 1112.2674656244137, -0.2465279459531899, 0.2675790139425493
+                nexus4, , camera, RADIAL, 2832, 2128, 2986.7801886485645, 1463.2779641275515, 1112.2674656244137, -0.2465279459531899, 0.2675790139425493
                 """)
 
             import_image_folder(
@@ -103,7 +103,7 @@ class TestImageFolder(unittest.TestCase):
         self.assertEqual(2, len(sensors))
         camera_ids = [s[0] for s in sensors]
         cameras = [s[1] for s in sensors]
-        self.assertEqual(['a', 'b'], camera_ids)
+        self.assertEqual(['milestone', 'nexus4'], camera_ids)
         self.assertTrue(kapture.Sensor, cameras[0])
         self.assertTrue(kapture.Sensor, cameras[1])
         self.assertTrue(kapture.Camera, cameras[0])
@@ -112,6 +112,6 @@ class TestImageFolder(unittest.TestCase):
         self.assertEqual(kapture.CameraType.RADIAL, cameras[1].camera_type)
         # images
         images = list(kapture.flatten(kapture_result.records_camera, is_sorted=True))
-        self.assertEqual(11, len(images))
+        self.assertEqual(4, len(images))
         first_image = images[0]
-        self.assertEqual((0, 'a', 'a/100_7100.JPG'), first_image)
+        self.assertEqual((0, 'milestone', 'milestone/2011-12-17_14-28-15_257.jpg'), first_image)
