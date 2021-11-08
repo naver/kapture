935008c598264e31ce6b76dfc3f69d55bfdd7092
julien.morat <julien.morat@naverlabs.com>
2021-11-08 16:41:19 +0100

    allow fix range for depth map to images.

--- tools/kapture_export_depth_images.py
+++ tools/kapture_export_depth_images.py
@@ -2,7 +2,7 @@
 # Copyright 2020-present NAVER Corp. Under BSD 3-clause license
 
 """
-Script to plot a kapture to a PLY file.
+Script to export a kapture depth maps to PNG files.
 """
 
 import argparse
@@ -16,10 +16,10 @@ import path_to_kapture  # noqa: F401
 import kapture
 import kapture.utils.logging
 import kapture.io.csv as csv
-import kapture.io.ply as ply
 import kapture.io.features
 from kapture.io.records import depth_map_from_file
-from typing import Optional, List
+from typing import Optional, Tuple
+import matplotlib.pyplot as plt
 
 logger = logging.getLogger('depth2images')
 
@@ -27,8 +27,15 @@ logger = logging.getLogger('depth2images')
 def export_depth_images(
         input_kapture_dir_path: str,
         output_depth_images_dir_path: str,
-        auto_depth_range: bool = True
+        depth_range: Optional[Tuple[float, float]] = None,
 ):
+    """
+
+    :param input_kapture_dir_path:
+    :param output_depth_images_dir_path:
+    :param depth_range: Optional depth range. If not given, automatically adjusted for each frame.
+    :return:
+    """
     logger.info('loading data ...')
     with csv.get_all_tar_handlers(input_kapture_dir_path) as tar_handlers:
         kapture_data = csv.kapture_from_dir(input_kapture_dir_path, tar_handlers=tar_handlers)
@@ -50,18 +57,23 @@ def export_depth_images(
         sensor_id = sensor_ids[depth_map_name]
         map_sizes = tuple(int(x) for x in kapture_data.sensors[sensor_id].sensor_params[1:3])
         depth_map = depth_map_from_file(depth_map_filepath, map_sizes)
-        # min max scaling
-        if auto_depth_range:
-            depth_map = (depth_map - depth_map.min()) / (depth_map.max() - depth_map.min())
+        if depth_range is None:
+            # min max
+            depth_range = (depth_map.min(), depth_map.max())
 
-        depth_map = (depth_map * 255.).astype(np.uint8)
+        assert depth_range is not None
+        # rescale range
+        depth_map = (depth_map - depth_range[0]) / (depth_range[1] - depth_range[0])
+        # save to 16bits single channel PNGs
+        depth_map = (depth_map * pow(255, 2)).astype(np.uint16)
         depth_image = Image.fromarray(depth_map)
         depth_image.save(output_png_filepath)
 
 
 def export_depth_images_command_line() -> None:
     """
-    Do the plot to ply file using the parameters given on the command line.
+    export a kapture depth maps to PNG files
+    using the parameters given on the command line.
     """
     parser = argparse.ArgumentParser(description=f'Export depth to images')
     parser_verbosity = parser.add_mutually_exclusive_group()
@@ -73,8 +85,11 @@ def export_depth_images_command_line() -> None:
         '-q', '--silent', '--quiet', action='store_const', dest='verbose', const=logging.CRITICAL)
     parser.add_argument('-i', '--input', required=True,
                         help='input path to kapture data root directory')
-    parser.add_argument('-o', '--output', required=False,
-                        help='output directory (PLY file format).')
+    parser.add_argument('-o', '--output',
+                        help='output directory. If not given, same as input.')
+    parser.add_argument('-r', '--range', nargs='+', type=float,
+                        help='min and max value. '
+                             'If not given, automatically computed for each frame.')
     args = parser.parse_args()
 
     logger.setLevel(args.verbose)
@@ -83,14 +98,19 @@ def export_depth_images_command_line() -> None:
         kapture.utils.logging.getLogger().setLevel(args.verbose)
     if not args.output:
         args.output = args.input
-
     args.input = path.abspath(args.input)
     args.output = path.abspath(args.output)
+    if args.range is not None and len(args.range) != 2:
+        parser.error(f'expect 2 floats as range values.')
+    args.range = tuple(args.range)
 
+    print(args.range)
     logger.debug(''.join(['\n\t{:13} = {}'.format(k, v) for k, v in vars(args).items()]))
     export_depth_images(
         input_kapture_dir_path=args.input,
-        output_depth_images_dir_path=args.output)
+        output_depth_images_dir_path=args.output,
+        depth_range=args.range
+    )
 
 
 if __name__ == '__main__':
