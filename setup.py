aa267bf56dc8f262e5ccb796df47fea7deef0f59
julien.morat <julien.morat@naverlabs.com>
2022-01-18 13:36:06 +0100

    version 1.1.6

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ long_description = read_doc(readme_filepath)
 setuptools.setup(
     # description
     name='kapture',
-    version="1.1.5",
+    version="1.1.6",
     author="naverlabs",
     author_email="kapture@naverlabs.com",
     description="kapture: file format for SfM",
