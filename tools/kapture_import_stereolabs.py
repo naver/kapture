3208a1a25ecfd77e6677749890ea497ae1ee26fb
jmorat <julien.morat@naverlabs.com>
2022-10-17 15:46:45 +0200

    fix zed cam import

--- tools/kapture_import_stereolabs.py
+++ tools/kapture_import_stereolabs.py
@@ -9,23 +9,19 @@ import argparse
 import logging
 import os
 import os.path as path
-import re
 import numpy as np
 import quaternion
 from typing import Optional
-from PIL import Image
 from tqdm import tqdm
 # kapture
 import path_to_kapture  # noqa: F401
 import kapture
 import kapture.utils.logging
-from kapture.io.structure import delete_existing_kapture_files
 from kapture.io.csv import kapture_to_dir
 import kapture.io.features
 from kapture.io.records import get_image_fullpath, get_depth_map_fullpath
 from kapture.io.binary import array_to_file
 
-from kapture.utils.paths import path_secure
 
 logger = logging.getLogger('kapture_import_zed')
 
@@ -54,6 +50,7 @@ def kapture_import_stereolabs(
         destination_kapture_dir_path: str,
         svo_file_path: str,
         sensors_file_path: Optional[str],
+        calib_dir_path: Optional[str],
         export_depth: bool = True,
 ) -> None:
     """
@@ -79,7 +76,7 @@ def kapture_import_stereolabs(
         kapture_data.records_depth = kapture.RecordsDepth()
 
     # Specify SVO path parameter
-    init_params = sl.InitParameters()
+    init_params = sl.InitParameters(optional_settings_path=calib_dir_path or '')
     init_params.set_from_svo_file(svo_file_path)
     init_params.svo_real_time_mode = False  # Don't convert in realtime
     init_params.coordinate_system = sl.COORDINATE_SYSTEM.IMAGE  # same as kapture
@@ -95,7 +92,7 @@ def kapture_import_stereolabs(
         # Open the SVO file specified as a parameter
         status = zed.open(init_params)
         if status != sl.ERROR_CODE.SUCCESS:
-            raise ImportError('unable to open cam')
+            raise ImportError(f'unable to open svo file {svo_file_path} ({status})')
 
         # calib
         configuration_sensors = zed.get_camera_information().sensors_configuration
@@ -141,6 +138,8 @@ def kapture_import_stereolabs(
         kapture_data.rigs[STEREOLABS_RIG_ID, STEREOLABS_CAM_IDS[0]] = kapture.PoseTransform()
         kapture_data.rigs[STEREOLABS_RIG_ID, STEREOLABS_CAM_IDS[1]] = kapture.PoseTransform(
             t=configuration_cameras.calibration_parameters.T)
+        if export_depth:
+            kapture_data.rigs[STEREOLABS_RIG_ID, STEREOLABS_DEPTH_ID] = kapture.PoseTransform()
 
         if sensors_file_path:
             # add the imu to rig config
@@ -155,7 +154,7 @@ def kapture_import_stereolabs(
         tracking_parameters = sl.PositionalTrackingParameters(_init_pos=sl.Transform())
         status = zed.enable_positional_tracking(tracking_parameters)
         if status != sl.ERROR_CODE.SUCCESS:
-            raise ImportError('unable to track')
+            raise ImportError(f'unable to track ({status})')
 
         # Prepare side by side image container equivalent to CV_8UC4
         # Prepare single image containers
@@ -173,99 +172,102 @@ def kapture_import_stereolabs(
 
         imu_file = open(sensors_file_path, 'rt') if sensors_file_path else None
         imu_lines = (l.split(',') for l in imu_file.readlines() if not l.startswith('#'))
-        for i in tqdm(range(nb_frames)):
-            status = zed.grab(runtime_parameters)
-            if status != sl.ERROR_CODE.SUCCESS:
-                raise ImportError()
+        try:  # slam loop
+            for i in tqdm(range(nb_frames)):
+                status = zed.grab(runtime_parameters)
+                if status == sl.ERROR_CODE.END_OF_SVOFILE_REACHED:
+                    logger.info(f'reaching end of file unexpectedly ({i}/{nb_frames})')
+                    break
 
-            svo_frame_idx = zed.get_svo_position()
-            assert svo_frame_idx == i
-            # Retrieve SVO images
-            status = zed.get_position(zed_pose,
-                                      sl.REFERENCE_FRAME.WORLD)  # Get the pose of the left eye of the camera with reference to the world frame
-            # ignore status, its faulty
-            timestamp_ns = zed_pose.timestamp.get_nanoseconds()
-
-            for cam_id, side in zip(STEREOLABS_CAM_IDS, [sl.VIEW.LEFT, sl.VIEW.RIGHT]):
-                status = zed.retrieve_image(image, side)
                 if status != sl.ERROR_CODE.SUCCESS:
-                    raise ImportError(f'unable to retrieve_image {cam_id}')
+                    raise ImportError(f'unable to grab data from svo file ({status})')
 
-                image_name = path.join(cam_id, f'{timestamp_ns}.png')
-                # records_camera[timestamp, sensor_id] = image_name
-                kapture_data.records_camera[timestamp_ns, cam_id] = image_name
+                svo_frame_idx = zed.get_svo_position()
+                assert svo_frame_idx == i
+                # Retrieve SVO images
+                status = zed.get_position(zed_pose,
+                                          sl.REFERENCE_FRAME.WORLD)  # Get the pose of the left eye of the camera with reference to the world frame
+                # ignore status, its faulty
+                timestamp_ns = zed_pose.timestamp.get_nanoseconds()
 
-                image_file_path = get_image_fullpath(kapture_dir_path=destination_kapture_dir_path,
-                                                     image_filename=image_name)
-                image.write(image_file_path)
+                for cam_id, side in zip(STEREOLABS_CAM_IDS, [sl.VIEW.LEFT, sl.VIEW.RIGHT]):
+                    status = zed.retrieve_image(image, side)
+                    if status != sl.ERROR_CODE.SUCCESS:
+                        raise ImportError(f'unable to retrieve_image {cam_id} ({status})')
 
-            if export_depth:
-                status = zed.retrieve_measure(depth_map, sl.MEASURE.DEPTH)  # Retrieve depth
-                if status != sl.ERROR_CODE.SUCCESS:
-                    raise ValueError(f'unable to retrieve_measure for depth.')
+                    image_name = path.join(cam_id, f'{timestamp_ns}.png')
+                    # records_camera[timestamp, sensor_id] = image_name
+                    kapture_data.records_camera[timestamp_ns, cam_id] = image_name
 
-                depth_name = path.join('Depth', f'{timestamp_ns}.depth')
-                kapture_data.records_depth[timestamp_ns, 'Depth'] = depth_name
-                depth_file_path = get_depth_map_fullpath(kapture_dir_path=destination_kapture_dir_path,
-                                                         depth_map_filename=depth_name)
-                # A .depth file is an array of float formatted as binary. The data type of the array is signed
-                # float on 32 bits. Each float value represent a distance in meters from the sensor origin plane.
-                depth_map_kapture = depth_map.get_data().astype(np.float32)
-                array_to_file(filepath=depth_file_path, data_array=depth_map_kapture)
+                    image_file_path = get_image_fullpath(kapture_dir_path=destination_kapture_dir_path,
+                                                         image_filename=image_name)
+                    image.write(image_file_path)
 
-            # if status != sl.ERROR_CODE.SUCCESS:
-            #     raise ExportError('unable to get_position')
-            # --> linear_acceleration returns correct values (norm = 9.8)
-            # --> angular_velocity is at 0 as expected because the SVO contains only data that have a meaning
-            # in image framerate (i.e. accelerometer + magnetometer)
-            # print(imu.get_angular_velocity(), imu.get_linear_acceleration())
-            pose_cam_from_world = zed_pose.pose_data(sl.Transform())
-            pose_cam_from_world.inverse()
-            orientation = pose_cam_from_world.get_orientation().get()  # x, y, z, w
-            translation = pose_cam_from_world.get_translation().get()  # x, y, z
-            # trajectories[(timestamp, sensor_id)] = < PoseTransform >
-            orientation = orientation[[3, 0, 1, 2]]  # reorder w, x, y, z
-            kapture_data.trajectories[timestamp_ns, STEREOLABS_RIG_ID] = kapture.PoseTransform(r=orientation,
-                                                                                               t=translation)
-            # grady on imu till we reach current timestamp
-            if imu_lines:
-                """
-                full sensor recording is :
-                    idx,imu_Timestamp[sec],mag_Timestamp[sec],baro_Timestamp[sec],
-                    accX[m/s^2],accY[m/s^2],accZ[m/s^2],
-                    gyroX[deg/s],gyroY[deg/s],gyroZ[deg/s],
-                    magX[uT],magY[uT],magZ[uT],
-                    orX[deg],orY[deg],orZ[deg],
-                    press[hPa],rel_alt[m],moving,
-                    temp_left[C],temp_right[C],temp_imu[C],temp_barom[C],
-                see : https://www.stereolabs.com/docs/gstreamer/zed-data-csv-sink/
-                """
-                for sensor_record in imu_lines:
-                    imu_timestamp_ns = int(float(sensor_record[1]) * 1e9)
-                    accelerometer_ms2 = [float(v) for v in sensor_record[4:7]]
-                    gyroscope_rads = [np.deg2rad(float(v)) for v in sensor_record[7:10]]
-                    kapture_data.records_accelerometer[
-                        imu_timestamp_ns, STEREOLABS_ACCELEROMETER_ID] = kapture.RecordAccelerometer(
-                        *accelerometer_ms2)
-                    kapture_data.records_gyroscope[imu_timestamp_ns, STEREOLABS_GYROSCOPE_ID] = kapture.RecordGyroscope(
-                        *gyroscope_rads)
-                    # TODO: read magneto, baro and co
-                    # read until the end or until timestamp is ahead of images
-                    if imu_timestamp_ns >= timestamp_ns:
-                        break
+                if export_depth:
+                    status = zed.retrieve_measure(depth_map, sl.MEASURE.DEPTH)  # Retrieve depth
+                    if status != sl.ERROR_CODE.SUCCESS:
+                        raise ImportError(f'unable to retrieve_measure for depth ({status}).')
 
-    except ImportError as e:
-        logger.critical(e)
-    except KeyboardInterrupt:
-        logger.critical('interrupted by user')
+                    depth_name = path.join('Depth', f'{timestamp_ns}.depth')
+                    kapture_data.records_depth[timestamp_ns, 'Depth'] = depth_name
+                    depth_file_path = get_depth_map_fullpath(kapture_dir_path=destination_kapture_dir_path,
+                                                             depth_map_filename=depth_name)
+                    # A .depth file is an array of float formatted as binary. The data type of the array is signed
+                    # float on 32 bits. Each float value represent a distance in meters from the sensor origin plane.
+                    depth_map_kapture = depth_map.get_data().astype(np.float32)
+                    array_to_file(filepath=depth_file_path, data_array=depth_map_kapture)
+
+                # if status != sl.ERROR_CODE.SUCCESS:
+                #     raise ExportError('unable to get_position')
+                # --> linear_acceleration returns correct values (norm = 9.8)
+                # --> angular_velocity is at 0 as expected because the SVO contains only data that have a meaning
+                # in image framerate (i.e. accelerometer + magnetometer)
+                # print(imu.get_angular_velocity(), imu.get_linear_acceleration())
+                pose_cam_from_world = zed_pose.pose_data(sl.Transform())
+                pose_cam_from_world.inverse()
+                orientation = pose_cam_from_world.get_orientation().get()  # x, y, z, w
+                translation = pose_cam_from_world.get_translation().get()  # x, y, z
+                # trajectories[(timestamp, sensor_id)] = < PoseTransform >
+                orientation = orientation[[3, 0, 1, 2]]  # reorder w, x, y, z
+                kapture_data.trajectories[timestamp_ns, STEREOLABS_RIG_ID] = kapture.PoseTransform(r=orientation,
+                                                                                                   t=translation)
+                # grady on imu till we reach current timestamp
+                if imu_lines:
+                    """
+                    full sensor recording is :
+                        idx,imu_Timestamp[sec],mag_Timestamp[sec],baro_Timestamp[sec],
+                        accX[m/s^2],accY[m/s^2],accZ[m/s^2],
+                        gyroX[deg/s],gyroY[deg/s],gyroZ[deg/s],
+                        magX[uT],magY[uT],magZ[uT],
+                        orX[deg],orY[deg],orZ[deg],
+                        press[hPa],rel_alt[m],moving,
+                        temp_left[C],temp_right[C],temp_imu[C],temp_barom[C],
+                    see : https://www.stereolabs.com/docs/gstreamer/zed-data-csv-sink/
+                    """
+                    for sensor_record in imu_lines:
+                        imu_timestamp_ns = int(float(sensor_record[1]) * 1e9)
+                        accelerometer_ms2 = [float(v) for v in sensor_record[4:7]]
+                        gyroscope_rads = [np.deg2rad(float(v)) for v in sensor_record[7:10]]
+                        kapture_data.records_accelerometer[
+                            imu_timestamp_ns, STEREOLABS_ACCELEROMETER_ID] = kapture.RecordAccelerometer(
+                            *accelerometer_ms2)
+                        kapture_data.records_gyroscope[imu_timestamp_ns, STEREOLABS_GYROSCOPE_ID] = kapture.RecordGyroscope(
+                            *gyroscope_rads)
+                        # TODO: read magneto, baro and co
+                        # read until the end or until timestamp is ahead of images
+                        if imu_timestamp_ns >= timestamp_ns:
+                            break
+        except KeyboardInterrupt:
+            logger.info('interrupted by user')
+        finally:
+            zed.disable_positional_tracking()
+            zed.close()
+
+        logger.info(f'imported kapture: {repr(kapture_data)}')
+        kapture_to_dir(kapture_dirpath=destination_kapture_dir_path, kapture_data=kapture_data)
+
     except Exception as e:
         logger.critical(e)
-    finally:
-        zed.disable_positional_tracking()
-        zed.close()
-
-    logger.info(f'imported kapture: {repr(kapture_data)}')
-    kapture_to_dir(kapture_dirpath=destination_kapture_dir_path, kapture_data=kapture_data)
 
 
 def kapture_import_zed_command_line() -> None:
@@ -287,6 +289,8 @@ def kapture_import_zed_command_line() -> None:
                         help='input path svo file')
     parser.add_argument('-u', '--imu',
                         help='input imu file')
+    parser.add_argument('-c', '--calib', default='',
+                        help='calib dir path (if not conventional)')
     parser.add_argument('-o', '--output', required=True, help='output directory.')
     ####################################################################################################################
     args = parser.parse_args()
@@ -299,7 +303,8 @@ def kapture_import_zed_command_line() -> None:
     kapture_import_stereolabs(
         destination_kapture_dir_path=args.output,
         svo_file_path=args.svo,
-        sensors_file_path=args.imu
+        sensors_file_path=args.imu,
+        calib_dir_path=args.calib
     )
 
 
